4c80d1c4fdedf51cddf04b029d9756cc708645d4
NikKonshin <konbshin1996@gmail.c0m>
2021-02-02 17:40:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/onmars/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.onmars
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
